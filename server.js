45155a818e4705b3efcd11e44c28a3cc1306f3e2
Sarah Stensberg <ladystensberg@gmail.com>
2018-11-28 09:57:18 -0800

    added server.js content and configured database.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+require('./config/database');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(require('method-override')('_method'));
+
+// define routes below
+// example: app.use('/api/fish', require('./routes/api'));
+
+// catch all route for react must come AFTER all other routes
+app.get('*', (req, res, next) => {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
