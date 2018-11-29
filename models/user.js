102533bcda0145e99ac225e06193254e7debe3bf
Sarah Stensberg <ladystensberg@gmail.com>
2018-11-28 16:04:14 -0800

    added auth

--- models/user.js
+++ models/user.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const statesList = [
 	'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC',
 	'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY',
 	'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT',
 	'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH',
-	'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 
+	'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX',
 	'UT', 'VT', 'VA', 'VI', 'WA', 'WV', 'WI', 'WY'
 ]
 
@@ -30,30 +31,51 @@ const phoneNumberSchema = new Schema({
 })
 
 const userSchema = new Schema({
-	firstName: {
+	name: {
 		type: String,
-		required: true
-	},
-	lastName: {
-		type: String,
-		required: true
+		required: [true, 'You must enter a name'],
+		minlength: [1, 'Name must be between 1 and 99 characters'],
+		maxlength: [99, 'Name must be between 1 and 99 characters']
 	},
 	email: {
 		type: String,
-		required: true,
-		unique: true,
-		lowercase: true
+		required: [true, 'You must enter an email'],
+		minlength: [5, 'Email must be between 5 and 99 characters'],
+		maxlength: [99, 'Email must be between 5 and 99 characters']
 	},
 	password: {
 		type: String,
-		required: true,
-		minlength: 8,
-		maxlength: 99
+		required: [true, 'You must enter a password'],
+		minlength: [10, 'Password must be between 10 and 128 characters'],
+		maxlength: [128, 'Password must be between 10 and 128 characters']
 	},
 	phoneNumbers: [phoneNumberSchema],
 	addresses: [addressSchema],
 	orders: [{ type: Schema.Types.ObjectId, ref: 'Order' }]
 
-}, {timestamps: true})
+}, { timestamps: true })
+
+userSchema.set('toObject', {
+	transform: function (doc, ret, options) {
+		let returnJson = {
+			_id: ret._id,
+			email: ret.email,
+			name: ret.name
+		}
+		return returnJson;
+	}
+});
+
+userSchema.methods.authenticated = function (password) {
+	return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.pre('save', function (next) {
+	if (this.isNew) {
+		let hash = bcrypt.hashSync(this.password, 12);
+		this.password = hash;
+	}
+	next();
+});
 
 module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const auth = require('./routes/auth');
 
 require('./config/database');
 require('dotenv').config();
@@ -10,12 +11,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(require('method-override')('_method'));
 
 // define routes below
 // example: app.use('/api/fish', require('./routes/api'));
+
+app.use('/auth', auth);
 
 // catch all route for react must come AFTER all other routes
 app.get('*', (req, res, next) => {
